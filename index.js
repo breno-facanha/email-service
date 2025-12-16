1ceff6a16f0130736440ade52c8dacba2c97197a
Breno Façanha <brenofacanha.ti@gmail.com>
2025-12-16 09:32:57 -0300

    criando microserviço de email

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4504 ;
+require('dotenv').config();
+
+
+app.use(cors({
+    origin: "localhost:4467",
+    methods: ["GET"],
+}));
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    const auth = req.headers.authorization;
+
+    if (!auth){
+        return res.status(401).send('Não autorizado');
+    }
+
+    if (auth !== process.env.SECRET_KEY){
+        return res.status(401).send('Chave Inválida');
+    }
+
+    console.log('Serviço executado com sucesso');
+  res.send('Serviço executado com sucesso');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
